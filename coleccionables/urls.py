fc5fc0052e0edc73f27bafb0cc546c1c90dd5e11
runo06 <161347274+runo06@users.noreply.github.com>
2025-09-24 20:40:06 -0600

    Cambio 2
    Cambio 2
    
    Co-Authored-By: DonYui <161346854+DonYui@users.noreply.github.com>
    Co-Authored-By: Alexis Coyotzi <161625984+AlexxThompson@users.noreply.github.com>

--- coleccionables/urls.py
+++ coleccionables/urls.py
@@ -4,6 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+<<<<<<< Updated upstream
+=======
+    path('', views.home , name='home'),
+    path('home/', views.home, name='home'),
+    path('busqueda/', views.busqueda, name='busqueda'),  
+    path('login/', views.user_login, name='login'),  
+    path('logout/', views.user_logout, name='logout'),
+    path('registro/', views.registro, name='registro'),
+>>>>>>> Stashed changes
     path('coleccionables/', views.lista_coleccionables, name='lista_coleccionables'),
     path('producto/<int:pk>/', views.producto_detalle, name='producto_detalle'),
     path('categoria/<slug:slug>/', views.productos_por_categoria, name='categoria'),
--- coleccionables/views.py
+++ coleccionables/views.py
@@ -7,6 +7,43 @@ def lista_coleccionables(request):
     productos = Coleccionable.objects.all()
     return render(request, 'lista_coleccionables.html', {'productos': productos})
 
+<<<<<<< Updated upstream
+=======
+def contactos(request):
+    return render(request, 'contactos.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def crear_coleccionable(request):
+    if request.method == 'POST':
+        form = ColeccionableForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_coleccionables')  # Redirige a la lista después de guardar
+    else:
+        form = ColeccionableForm()
+    
+    return render(request, 'crear_coleccionable.html', {'form': form})
+
+def home(request):
+    productos_destacados = Coleccionable.objects.all()[:10]
+    categorias = Categoria.objects.all()[:4]  
+    return render(request, 'home.html', {'productos': productos_destacados, 'categorias': categorias})
+
+def busqueda(request):
+    query = request.GET.get('q')
+    coleccionables = Coleccionable.objects.all()
+    
+    if query:
+        coleccionables = coleccionables.filter(titulo__icontains=query)
+
+    return render(request, 'busqueda.html', {
+        'coleccionables': coleccionables,
+        'query': query
+    })
+
+>>>>>>> Stashed changes
 def producto_detalle(request, pk):
     producto = get_object_or_404(Coleccionable, pk=pk)
     return render(request, 'producto_detalle.html', {'producto': producto})
